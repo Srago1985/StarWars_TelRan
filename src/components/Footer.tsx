e5d51970724b11f78f1a3afcbb8c20b7023cddf1
Srago1985 <mihanik999@gmail.com>
2026-09-07 20:19:57 +0300

    Link email button to contact form

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router";
+import { appRoutes } from "../utils/constants.ts";
+
 const Footer = () => {
     return (
         <footer className="clear-both rounded-b-3xl bg-gray h-20 grid grid-cols-10 items-center">
-            <div className='bg-danger rounded-md px-3 border cursor-pointer hover:bg-red-500 hover:text-amber-50 col-start-4 col-span-2 text-center'>Send me email</div>
+            <Link to={appRoutes.contact} className='bg-danger rounded-md px-3 border cursor-pointer hover:bg-red-500 hover:text-amber-50 col-start-4 col-span-2 text-center'>Send me email</Link>
         </footer>
     )
 }
